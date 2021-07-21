85ebe56b0b30216435397993f957d05b3cb41384
Shrestha Kumar <shresthakumar371@gmail.com>
2021-07-22 01:08:49 +0530

    Create Swapping_pairs_from_two_arrays_which_makes_sum_equal.cpp
    Find a pair of elements from two arrays respectively such that after swapping them makes sum of two arrays equal.

---
+++ Arrays/Swapping_pairs_from_two_arrays_which_makes_sum_equal.cpp
@@ -0,0 +1,65 @@
+int findSwapValues(int A[], int n, int B[], int m)
+{
+	    unordered_map<int, int>hash_map;
+	    
+	    int sumA = 0, sumB = 0;
+	    
+	    for(int i = 0; i < n; i++)
+	        sumA += A[i];
+	       
+	    for(int i = 0; i < m; i++)
+	        sumB += B[i];
+	    
+	    if(sumA == sumB)
+	    {
+	         for(int i = 0; i < n ;i++)
+	            hash_map[A[i]] = 1;
+	         
+	         for(int i = 0; i < m; i++)
+	         {
+	             if(hash_map[B[i]] == 1)
+	                return 1;
+	         }
+	         
+	         return -1;
+	    }
+	        
+	    int arr_max = (sumA > sumB)?1:2;
+	    int check_val = 0;
+	    
+	    if(arr_max == 1)
+	    {
+	        for(int i = 0; i < n ;i++)
+	            hash_map[A[i]] = 1;
+	        if((sumA - sumB)%2 == 1)
+	            return -1;
+	        check_val = (sumA - sumB)/2;
+	    }
+	    else if(arr_max == 2)
+	    {
+	        for(int i = 0; i < m; i++)
+	             hash_map[B[i]] = 1;
+	        if((sumB - sumA)%2 == 1)
+	            return -1;
+	       check_val = (sumB - sumA)/2;
+	    }
+	    
+	    if(arr_max == 1)
+	    {
+	        for(int i = 0; i < m; i++)
+	        {
+	            if(hash_map[check_val + B[i]])
+	                return 1;
+	        }
+	    }
+	    else if(arr_max == 2)
+	    {
+	        for(int i = 0; i < n; i++)
+	        {
+	            if(hash_map[check_val + A[i]])
+	                return 1;
+	        }
+	    }
+	    
+	    return -1;
+	}
